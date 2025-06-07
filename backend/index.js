e8087814738f61e25a77efa4a54a650d2a359fc3
reza pratama <rezap9303@gmail.com>
2025-06-07 15:23:59 +0900

    fixing code

--- backend/index.js
+++ backend/index.js
@@ -2,38 +2,44 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import { connectDB } from './config/database.js';
+
 import tourRoute from './routes/tours.js';
 import userRoute from './routes/users.js';
 import authRouter from './routes/auth.js';
 import reviewRoute from './routes/reviews.js';
 import bookingRoute from './routes/booking.js';
 
-dotenv.config()
-const app = express()
-const port = process.env.PORT || 8000
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8000;
 
+// CORS
 app.use(cors({
   origin: process.env.PORT_FE,
   credentials: true
 }));
 
-
-// middleware
+// Middleware
 app.use(express.json());
-app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/hello", (req, res) => {
+// Endpoint test
+app.get("/app2/api/hello", (req, res) => {
   res.json({ message: "Hello from backend!" });
 });
 
-app.use(authRouter)
-app.use(tourRoute)
-app.use(userRoute)
-app.use(reviewRoute)
-app.use(bookingRoute)
+const app2Router = express.Router();
 
-app.listen(port, ()=> {
-    connectDB()
-    console.log('server up and running on port', port)
-})
+app2Router.use("/api/auth", authRouter);
+app2Router.use("/api/tours", tourRoute);
+app2Router.use("/api/users", userRoute);
+app2Router.use("/api/reviews", reviewRoute);
+app2Router.use("/api/bookings", bookingRoute);
+
+app.use("/app2", app2Router);
+
+// Start server
+app.listen(port, () => {
+  connectDB();
+  console.log('Server up and running on port', port);
+});
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/tour-frontend/',
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
 })
